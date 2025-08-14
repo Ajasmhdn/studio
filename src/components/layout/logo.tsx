c602fbf97c9c38f48ca09910c3ab1c1223e3c01c
Ajas Muhammed N <ajasaju404@gmail.com>
2025-08-14 17:30:18 +0000

    still not visiblei want the logo to there

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center gap-2 group shrink-0">
       <Image
-        src="https://lh3.googleusercontent.com/IfnByc-AfW50UuaWlZhf5RNH_jwYCiUkdJsCeHrET40iy_h33fBPprHcG6_LClppjDK1f-dspN9E4litvZDDlruBdDY-0zLkKDoV-FTU6AwzTPrqbUir-SfQsWlg1sUk2oxK8agk2sk=w16383"
+        src="https://i.ibb.co/jrh20B7/tkmce-logo.png"
         alt="TKMCE AI Hub Logo"
         width={180}
         height={40}
